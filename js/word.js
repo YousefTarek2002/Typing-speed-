b6ddd67efffeff04588e9142ef19375e059f25cc
YousefTarek2002 <84736128+YousefTarek2002@users.noreply.github.com>
2022-10-17 19:12:56 +0200

    Add files via upload

---
+++ js/word.js
@@ -0,0 +1,72 @@
+let words = [
+    {
+        word: "meeting",
+    },
+    {
+        word: "number",
+    },
+    {
+        word: "fast",
+    },
+    {
+        word: "canvas",
+    },
+    {
+        word: "garden",
+    },
+    {
+        word: "position",
+    },
+    {
+        word: "comfort",
+    },
+    {
+        word: "tongue",
+    },
+    {
+        word: "home",
+    },
+    {
+        word: "country",
+    },
+    {
+        word: "meeting",
+    },
+    {
+        word: "number",
+    },
+    {
+        word: "exchange",
+    },
+    {
+        word: "canvas",
+    },
+    {
+        word: "garden",
+    },
+    {
+        word: "position",
+    },
+    {
+        word: "comfort",
+    },
+    {
+        word: "tongue",
+    },
+    {
+        word: "expansion",
+    },
+    {
+        word: "beginner",
+    },
+    {
+        word: "better",
+    },
+    {
+        word: "carefully",
+    },
+    {
+        word: "certainly",
+    },
+    
+]
